1ab8932c1079159fc161838862ef389b00df08ba
Dom-Vibecoder <domeniccarigiet@outlook.de>
2026-02-17 11:01:25 +0100

    New updated files
    New files with fixes for icon dissapearance

--- eventspot-v2_1/sw.js
+++ eventspot-v2_1/sw.js
@@ -1,21 +1,5 @@
-const CACHE = 'eventspot-v2';
-const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  // Network first for API calls, cache first for static assets
-  if (e.request.url.includes('firestore') || e.request.url.includes('googleapis')) {
-    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-  } else {
-    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-  }
-});
+var CACHE='eventspot-v3';
+var ASSETS=['/','index.html','manifest.json','icon-192.png','icon-512.png'];
+self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS)}));self.skipWaiting()});
+self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(k){return Promise.all(k.filter(function(x){return x!==CACHE}).map(function(x){return caches.delete(x)}))}));self.clients.claim()});
+self.addEventListener('fetch',function(e){if(e.request.url.indexOf('firestore')>=0||e.request.url.indexOf('googleapis')>=0){e.respondWith(fetch(e.request).catch(function(){return caches.match(e.request)}))}else{e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request)}))}});
